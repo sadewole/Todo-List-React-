cc473a6b100c63b5c33c60bb126c7f4e167a6f8c
Samuel Adewole <samueladewole15@gmail.com>
2019-07-03 22:59:20 +0100

    init project commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoInput from './component/Todoinput';
+import TodoList from './component/TodoList';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
 
 class App extends Component {
+  state = {
+    items: [],
+    id: uuid(),
+    item: '',
+    editItem: false
+  };
+
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+
+    const updatedItem = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItem,
+      item: '',
+      id: uuid(),
+      editItem: false
+    });
+  };
+
+  clearList = () => {
+    this.setState({
+      items: []
+    });
+  };
+
+  handleDelete = id => {
+    const filteredDelete = this.state.items.filter(item => item.id !== id);
+
+    this.setState({
+      items: filteredDelete
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto col-md-8 mt-4'>
+            <h3 className='text-capitalize text-center'>todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+            />
+          </div>
+        </div>
       </div>
     );
   }
